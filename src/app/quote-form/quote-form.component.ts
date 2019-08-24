b43a8547ce2e8266d354cc0d06540a8c72c3dfa6
Ruzindana Wendy Ornella <ruzindanawendyornella@Ruzindanas-MacBook-Pro.local>
2019-08-24 19:21:51 +0200

    updated the add new quote button

---
+++ src/app/quote-form/quote-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Output, EventEmitter,AfterViewInit} from '@angular/core';
+import * as $ from 'jquery';
+import { Quote} from '../quote';
+
+ 
+
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
+  newQuote = new Quote("","");
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitGoal(){
+this.addQuote.emit(this.newQuote);
+this.newQuote=new Quote("","")
+
+// if(this.newQuote!==null){
+  // $('button#me').click(function(){
+  //   // $(alert('hi'));
+  //   console.log(this.newQuote)
+
+  // $('ul#list').append("<li><span>"+this.newQuote.name+"</span></li>");
+  // });
+// }
+  }
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+  }
+
