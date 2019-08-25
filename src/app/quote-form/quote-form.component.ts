3d4582aff231b438001c1231167f0438616562b9
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-08-25 12:28:42 +0200

    corrected some indentation errors

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import { Component, OnInit, Output, EventEmitter,AfterViewInit} from '@angular/core';
 import * as $ from 'jquery';
 import { Quote} from '../quote';
-
- 
-
-=======
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
-import { Quote} from '../quote';
->>>>>>> 9f05a7e1d6c7d8783bcc14e96395d4ee6a92e46c
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
@@ -20,7 +12,6 @@ export class QuoteFormComponent implements OnInit {
 
   submitGoal(){
 this.addQuote.emit(this.newQuote);
-<<<<<<< HEAD
 
 this.newQuote=new Quote("","");
 
@@ -32,19 +23,10 @@ this.newQuote=new Quote("","");
   // $('ul#list').append("<li><span>"+this.newQuote.name+"</span></li>");
   // });
 // }
-=======
->>>>>>> 9f05a7e1d6c7d8783bcc14e96395d4ee6a92e46c
+
   }
   constructor() { }
 
-  ngOnInit() {
-<<<<<<< HEAD
-    
-  }
-  }
-
-=======
-  }
+  ngOnInit() {  }
 
 }
->>>>>>> 9f05a7e1d6c7d8783bcc14e96395d4ee6a92e46c
