fa0ad817543e45355754f1f7629a3fa3c2e8231c
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-08-25 13:10:15 +0200

    re-updated file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,9 @@ import { Quote} from './quote'
 export class AppComponent {
   quote='Welcome'
   quotes:Quote []=[
-  new Quote('"Words are pale shadows of forgotten names. As names have power, words have power. Words can light fires in the minds of men. Words can wring tears from the hardest hearts."' ,'Patrick Rothfuss'),
-  new Quote('"Imperfection is beauty, madness is genius and it is better to be absolutely ridiculous than absolutely boring."','Marilyn Monroe'),
-  new Quote('"We are all in the gutter, but some of us are looking at the stars."','Oscar Wilde')
+  new Quote('Words are pale shadows of forgotten names. As names have power, words have power. Words can light fires in the minds of men. Words can wring tears from the hardest hearts.' ,'Patrick Rothfuss','nick'),
+  new Quote('Imperfection is beauty, madness is genius and it is better to be absolutely ridiculous than absolutely boring.','Marilyn Monroe','cynthia'),
+  new Quote('We are all in the gutter, but some of us are looking at the stars.','Oscar Wilde','ornella')
   ]
   addNewQuote(quote){
     let quoteLength = this.quotes.length;
